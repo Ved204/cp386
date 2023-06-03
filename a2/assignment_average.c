91a338500769369ff6e4a7510bca2baa4b8ddbbd
Ved Patel <patelved44@outlook.com>
2023-06-03 20:35:16 +0000

    file

--- a2/assignment_average.c
+++ a2/assignment_average.c
@@ -1,7 +1,27 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
+int main(FILE *file){
 
+    FILE *fp;
+    char * line = NULL;
+    fp = fopen(file, "r");
+    int lines = 0;
+    int matrix[] = {};
+    size_t len = 0;
+    ssize_t read;
+
+    if (fp == NULL) {
+        printf("Error: file pointer is null.");
+        return -1;
+    }
+
+    while ((read = getline(&line, &len, fp)) != -1) {
+        printf("Retrieved line of length %zu:\n", read);
+        printf("%s", line);
+    }
+
+    fclose(fp);
 
 }
