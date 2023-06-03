147303d501ef9b6eafaa188239f2836f8b138938
Ved Patel <patelved44@outlook.com>
2023-06-03 21:51:10 +0000

    lines

--- a2/assignment_average.c
+++ a2/assignment_average.c
@@ -4,14 +4,16 @@
 #define MAX_SIZE 100
 
 int main(){
-
+    size_t len = 0;
+    ssize_t read;
     FILE *fp;
-    FILE *input;
+    FILE *file;
     char * line = NULL;
     int number;
     int lines = 0;
     int matrix[MAX_SIZE] = {};
     fp = fopen("sample_in_grades.txt", "r");
+    file = fopen("sample_in_grades.txt", "r");
 
     if (fp == NULL) {
         printf("Error: file pointer is null.");
@@ -21,10 +23,15 @@ int main(){
     int i = 0;
     while (fscanf(fp, "%d", &number) != EOF) {
         // Process the number here (e.g., print it)
-        // printf("%d\n", number);
         matrix[i] = number;
         i++;
     }
+
+    while ((read = getline(&line, &len, file)) != -1) {
+        lines++;
+    }
+
+    printf("%d\n", lines);
 
     for (int j = 0; j < i; j++) {
         printf("%d ", matrix[j]);
@@ -32,5 +39,5 @@ int main(){
 
     fclose(fp);
 
-    return  0;
+    return 0;
 }
