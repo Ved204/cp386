6fa6d5b62db7cbb0c4e1b1757c4ac55813aa8dca
Mann Patel <mannhp1103@outlook.com>
2023-06-05 19:21:00 -0400

    Understanding wait

--- a2/process_management.c
+++ a2/process_management.c
@@ -4,7 +4,7 @@
 #include <unistd.h>
 
 int main(int argc, char* argv[]){
-    // fork() splits the process into a main process and child process 
+    // fork() function splits the process into a main process and child process 
     // if int id = fork() == 0 then we are in the child process
     // if int id = fork() != 0 then we are in the main process
     /* for example if we did "fork()" and then "printf("Hello world\n")" in the next line,
@@ -13,7 +13,11 @@ int main(int argc, char* argv[]){
        print hello world 2^3 times (8 times).
     */
 
-    
+    // wait() function stops the execution of main process until a child process has finished executing
+    // after child process has finished executing, the main process will continue
+    // the function is very helpful when you try to print parts of things from one process and part of things from another process
+    // in an ordered manner 
+
 
 
     return 0;
