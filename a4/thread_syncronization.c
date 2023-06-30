a287e960baeb7bd09ed67f44abba67c38f553174
Mann Patel <mannhp1103@outlook.com>
2023-06-30 19:44:34 -0400

    thread_synchronization

--- a4/thread_syncronization.c
+++ a4/thread_syncronization.c
@@ -16,6 +16,7 @@
 #include <pthread.h>
 #include <sys/stat.h>
 #include <time.h>
+#include <semaphore.h>
 
 sem_t running;
 sem_t even;
@@ -35,6 +36,7 @@ typedef struct thread //represents a single thread, you can add more members if 
 	int state;
 	pthread_t handle;
 	int retVal;
+    int no_wait;
 } Thread;
 
 //you can add more functions here if required
@@ -55,6 +57,19 @@ int main(int argc, char *argv[]) {
 	Thread *threads = NULL;
 	int threadCount = readFile(argv[1], &threads);
 
+    int c_odd = 0;
+    int c_even = 0;
+
+    if ((atoi(threads[0].tid+1) % 2) != 0){
+        c_odd = 1;
+    }
+    else{
+        c_even = 1;
+    }
+
+    sem_init(&odd, 0, c_odd);
+    sem_init(&even, 0, c_even);
+
 	startClock();
 	while (threadsLeft(threads, threadCount) > 0) //put a suitable condition here to run your program
 	{
@@ -65,6 +80,28 @@ int main(int argc, char *argv[]) {
 			//you can add some suitable code anywhere in this loop if required
 
 			threads[i].state = 1;
+
+            int count_odd = 0;
+            int count_even = 0;
+
+            for (int i = 0; i < threadCount; i++){
+                if (threads[i].state != -1){
+                    if ((atoi(threads[i].tid + 1) % 2) != 0){
+                        count_odd++;
+                    }
+                    else{
+                        count_even++;
+                    }
+                }
+            }
+
+            if (count_odd > 1 && count_even == 0){
+                threads[i].no_wait = 1;
+            }
+            else if (count_even > 1 && count_odd == 0){
+                threads[i].no_wait = 1;
+            }
+
 			threads[i].retVal = pthread_create(&(threads[i].handle), NULL,
 					threadRun, &threads[i]);
 		}
@@ -124,6 +161,7 @@ int readFile(char *fileName, Thread **threads) //do not modify this method
 //you can do initialization of those additional members
 //or any other action on the Thread members
 			(*threads)[k].state = 0;
+            (*threads)[k].no_wait = 0;
 			if (j == 0)
 				strcpy((*threads)[k].tid, token);
 			if (j == 1)
@@ -164,17 +202,46 @@ void* threadRun(void *t) //implement this function in a suitable way
 {
 	logStart(((Thread*) t)->tid);
 
-//your synchronization logic will appear here
+    int c_even = 5;
+    int c_odd = 5;
+    int id = atoi(((Thread*) t)->tid + 1);
+
+    sem_getvalue(&odd, &c_odd);
+    sem_getvalue(&even, &c_even);
+
+    if (((Thread*) t)->no_wait == 0){
+        if ((id % 2) != 0){
+            sem_wait(&odd);
+        }
+        else{
+            sem_wait(&even);
+        }
+    }
+
 
 	//critical section starts here
 	printf("[%ld] Thread %s is in its critical section\n", getCurrentTime(),
 			((Thread*) t)->tid);
 	//critical section ends here
 
-//your synchronization logic will appear here
 
 	logFinish(((Thread*) t)->tid);
 	((Thread*) t)->state = -1;
+
+    if ((id % 2) == 1){
+        sem_post(&even);
+        if(((Thread*) t)->no_wait == 1){
+            sem_post(&odd);
+        }
+    }
+
+    if ((id % 2) == 0){
+        sem_post(&odd);
+        if (((Thread*) t)->no_wait == 1){
+            sem_post(&even);
+        }
+    }
+
 	pthread_exit(0);
 }
 
