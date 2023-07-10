a127625a0129234836948bae11af6cf11e17aa04
Mann Patel <mannhp1103@outlook.com>
2023-07-10 11:48:46 -0400

    banker.c intialization

---
+++ a5/banker.c
@@ -0,0 +1,18 @@
+/*
+ -------------------------------
+ Assignment 5
+ File: banker.c
+ -------------------------------
+ Author:  Mann Patel / Ved Patel
+ ID:      210852760 / 210304140
+ Email:   pate5276@mylaurier.ca / pate4140@mylaurier.ca
+ Version: 2023-07-10
+ -------------------------------
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    return 0;
+}
